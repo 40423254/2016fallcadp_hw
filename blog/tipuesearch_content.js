a3e93e9f03637cf8cf6d2c5d88ef6e22dafdc7ed
40423254 <40423254@gm.nfu.edu.tw>
2016-12-16 10:42:46 +0800

    rm data test

--- blog/tipuesearch_content.js
+++
@@ -1,1 +0,0 @@
-var tipuesearch = {"pages":[{"tags":"misc","text":"2016Fall 修課成員網誌","url":"./pages/about/","title":"About"},{"tags":"Misc","text":"這個系統共集結了 reveal.js 網際簡報與 pelican 靜態網誌系統. 網誌 Category 網誌 Tags reveal.js 使用導引","url":"./2016fall-jian-bao-yu-wang-zhi-xi-tong.html","title":"2016Fall 簡報與網誌系統"}]};
